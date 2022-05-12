030ca49da903e92753f0559d67306baadd9341d7
JDPowell648 <41934552+JDPowell648@users.noreply.github.com>
2022-05-11 23:07:24 -0500

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -2,19 +2,11 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  user: "root",
-  password: "62b89689a6f847cc", //62b89689a6f847cc
-  host: "coms-319-g16.class.las.iastate.edu", //coms-319-g16.class.las.iastate.edu
-  port: 3306,
+  user: "",
+  password: "", 
+  host: "", 
+  port: ,
   database: "geogame",
 });
 
-const testPool = mysql.createPool({
-  user: "root",
-  password: "62b89689a6f847cc", //62b89689a6f847cc
-  host: "coms-319-g16.class.las.iastate.edu",
-  port: 3306,
-  database: "testdb",
-});
-
-module.exports = { pool, testPool };
+module.exports = { pool };
