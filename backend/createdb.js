b077cb156a911e1f31a190715b8201ff914f9f81
JDPowell648 <41934552+JDPowell648@users.noreply.github.com>
2022-05-11 23:09:50 -0500

    Update createdb.js

--- backend/createdb.js
+++ backend/createdb.js
@@ -1,6 +1,6 @@
-const host = "coms-319-g16.class.las.iastate.edu";
-const user = "root";
-const password = "62b89689a6f847cc";
+const host = "";
+const user = "";
+const password = "";
 
 const Importer = require("mysql-import");
 const importer = new Importer({ host, user, password });
